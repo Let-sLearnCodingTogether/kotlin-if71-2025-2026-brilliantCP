8c7ea81cde6b9706df6a6f55e98692757698478c
brilliantCP <132752928+brilliantCP@users.noreply.github.com>
2025-09-18 20:41:02 +0700

    Create: OperatorPerbandingan, OperatorBoolean
    Studying about
    Operator Perbandingan
    Operator Boolean

---
+++ BasicKOTLIN/src/OperatorPerbandingan.kt
@@ -0,0 +1,19 @@
+fun main() {
+    val nilaiA = 20
+    val nilaiB = 15
+
+    val apakahLebihBesar = nilaiA > nilaiB
+    println(apakahLebihBesar)
+
+    val apakahSama = nilaiA == nilaiB
+    println(apakahSama)
+
+    val apakahTidakSama = nilaiA != nilaiB
+    println(apakahTidakSama)
+
+    val apakahLebihKecil = nilaiA < nilaiB
+    println(apakahLebihKecil)
+
+    println("apel" < "kacang") //ascii table based
+
+ }
